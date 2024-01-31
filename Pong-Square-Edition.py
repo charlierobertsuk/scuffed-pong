fdf81192ab186c308e89c00e97bae6e6d87335e6
Charlie Roberts <charlie@robertsclan.co.uk>
2024-01-31 15:00:56 +0000

    square file

--- Pong-Square-Edition.py
+++ Pong-Square-Edition.py
@@ -1,9 +1,9 @@
-# This is pong
+# Hello!
 
 # Import
-import pygame, sys, random
+import pygame, sys
 
-# Screen size
+#  Screen size
 screen_width = 1000
 screen_height = 500
 half_width = screen_width // 2
@@ -28,16 +28,6 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 0)
 AQUA = (0, 255, 255)
-PINK = ("pink")
-PURPLE = (99, 3, 48)
-DARKGREEN = (1, 50, 32)
-ORANGE = (255, 68, 51) 
-
-# Colour change on colision variables
-background_colour_change = [KYLE, PURPLE, GREEN, YELLOW]
-ball_colour_change = [AQUA, DARKGREEN, ORANGE, PINK]
-i = 0
-j = 0
 
 # Surfboard variables
 surf_x = 15
@@ -46,7 +36,7 @@ surf_y = surf_x * 6
 # Right surfboard
 right_surfboard = pygame.Rect(900, 200, surf_x, surf_y)
 
-# Right surfboard margin - so I can control collisions with a single axis and not just with whe surfboard - prevents ball entering surfboard from top and bouncing through
+# Right surfboard margin
 top_right_surfboard = pygame.Rect(900, 200, surf_x, 2)
 bottom_right_surfboard = pygame.Rect(900, 200 + (surf_y - 2), surf_x, 2)
 
@@ -57,11 +47,9 @@ left_surfboard = pygame.Rect(100, 200, surf_x, surf_y)
 top_left_surfboard = pygame.Rect(100, 200, surf_x, 2)
 bottom_left_surfboard = pygame.Rect(100, 200 + (surf_y - 2), surf_x, 2)
 
-#NOTE: Make left margin and make all margins move with the surfboard then after that, sort collisions :)
-
 # Ball variables
 ballsize = 20
-ballspeed = 5
+ballspeed = 6
 ballspeed_x = ballspeed
 ballspeed_y = ballspeed
 ball = pygame.Rect(half_width, half_height, ballsize, ballsize)
@@ -104,56 +92,26 @@ while True:
     ball.x += ballspeed_x
     ball.y += ballspeed_y
 
-    # Colour change loop
-    if i != 3:
+    # Wall colision check
+    if ball.top <= 0 or ball.bottom >= screen_height: # if ball hits top or bottom of screen
+        ballspeed_y = -ballspeed_y # move in oppisite y direction
 
-        # Wall colision check
-        if ball.top <= 0 or ball.bottom >= screen_height: # if ball hits top or bottom of screen
-            ballspeed_y = -ballspeed_y # move in oppisite y direction
-            i += 1
-            if i == 3:
-                i = 0
+    if ball.left <= 0 or ball.right >= screen_width: # if ball hits left or right of screen
+        ballspeed_x = -ballspeed_x # move in oppisite x direction
 
-        if ball.left <= 0 or ball.right >= screen_width: # if ball hits left or right of screen
-            ballspeed_x = -ballspeed_x # move in oppisite x direction
-            i += 1
-            if i == 3:
-                i = 0
+    # Background colour
+    screen.fill(AQUA)
 
-        # Background colour
-        screen.fill(background_colour_change[i])
-
-    if j != 3:
-
-        #shape = random.randint (1, 1)
-        shape = 2
-        if shape == 1:
-            # Draw ball (circle)
-            pygame.draw.circle(screen, ball_colour_change[i], (ball.x, ball.y), 10, 10) #NOTE: Ball collides half way when left colision - pls fix
-
-        if shape == 2:
-            # Draw ball (square)
-            pygame.draw.rect(screen, ball_colour_change[j], ball)
-
-        if shape == 3:
-            # Draw ball (triangle)
-            pygame.draw.polygon(screen, ball_colour_change[j], ((25,75),(320,125),(250,375)))
+    # Draw Ball
+    pygame.draw.rect(screen, KYLE, ball)
 
     # Surfboard collision check x
     if ball.colliderect(left_surfboard or left_surfboard) or ball.colliderect(right_surfboard or right_surfboard):
-        ballspeed_x = -ballspeed_x
-        j +=1
-        if j == 3:
-            j = 0
+        ballspeed_x = -ballspeed_x * 1.01
 
     # Surfboard colision check y - if ball hits top or bottom of surfboard then bounce NOTE: Dosen't work yet :(
     if ball.colliderect(top_left_surfboard or bottom_left_surfboard) or ball.colliderect(top_right_surfboard or bottom_right_surfboard):
-        ballspeed_y = -ballspeed_y
-        j += 1
-        if j == 3:
-            j = 0
-    
-# NOTE: fix if statement
+        ballspeed_y = -ballspeed_y * 1.01
 
     # Draw surfboards
     pygame.draw.rect(screen, BLUE, left_surfboard)
@@ -173,3 +131,5 @@ while True:
     # Set game fps
     clock.tick(60)
 
+    #make different files for different shapes
+    #make a file for cpu that folows the balls y coordinate or somthin
